41b9976db3fe736757a7054cc5329f6b18bf5111
bothazerty12 <bouthaynaakrouti522@gmail.com>
2019-11-12 16:54:55 +0100

    Updates

---
+++ precache-manifest.e4bea4a414eaa5b81774dab90e2e5a80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32a9eeb5682dd352d27983bfc5a93d8",
+    "url": "/Redux-Movie-App/index.html"
+  },
+  {
+    "revision": "d193e5fd2617b115862f",
+    "url": "/Redux-Movie-App/static/css/main.02ff69d7.chunk.css"
+  },
+  {
+    "revision": "c73c81d41c9a5024e13d",
+    "url": "/Redux-Movie-App/static/js/2.fc233107.chunk.js"
+  },
+  {
+    "revision": "d193e5fd2617b115862f",
+    "url": "/Redux-Movie-App/static/js/main.d3ec4154.chunk.js"
+  },
+  {
+    "revision": "dd60c24a2365b6728a2e",
+    "url": "/Redux-Movie-App/static/js/runtime-main.07bd5388.js"
+  }
+]);
